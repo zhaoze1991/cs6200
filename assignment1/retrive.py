6fe267b964ffc72b4a8a5f31b14811f23d249274
zhaoze1991 <zhaoze.chou@gmail.com>
2015-01-25 02:52:46 -0500

    Handle document length
    This commit make sure the way to calculate
    document length, V, and average document length
    is correct.

--- assignment1/retrive.py
+++ assignment1/retrive.py
@@ -12,12 +12,61 @@ es = Elasticsearch()
 stopwords = indexing.readFile('./AP_DATA/stoplist.txt')
 D = es.count(index = 'ap_dataset')['count'] # D is the total number of documents
 K = 100                        # K is the number of documents should be returned
+V = 10000000
+average = 200.0
+document_length = {}
+all_item = []
 class Document(object):
     """docstring for Document"""
     def __init__(self, the_id, info):
         super(Document, self).__init__()
         self.id = the_id
         self.information = info
+# ----------------------------------------------------------
+def getNeededInformation():
+    _id = 0
+    avg = 0
+    global D
+    while _id < D :
+        term = es.termvector(index = 'ap_dataset', doc_type='document', id = _id)
+        if 'text' not in term['term_vectors']:
+            _id += 1
+            continue
+        term = term['term_vectors']['text']['terms']
+        total = 0
+        # get the length of the document
+        for key in term.items():
+            freq = key[1]['term_freq']
+            total += freq
+        term = term.keys()
+        for t in term:
+            t = t.encode('UTF-8')
+            if t in all_item:
+                continue
+            all_item.append(t)
+            # print t
+        text = es.search(index = 'ap_dataset', doc_type = 'document',
+            body = {
+            'query' : {
+            'match_phrase':{
+            '_id':{'query':_id}
+            }}})
+        text = text['hits']['hits'][0]['_source']
+        docno = text['docno'].encode('UTF-8')
+        document_length[docno] = total
+        avg += total
+        _id += 1
+    global average
+    average = avg / D
+    global V
+    V =len(all_item)
+    save = open('cache','w')
+    save.writelines(str(V) +'\n')
+    save.writelines(str(average) + '\n')
+    for key in document_length:
+        string = key + ' ' + str(document_length[key])+'\n'
+        save.writelines(string)
+    save.close()
 # ----------------------------------------------------------
 def getInformation(keyword, result):
     df = result['hits']['total']
@@ -26,25 +75,21 @@ def getInformation(keyword, result):
     num = 0
     for hit in result['hits']['hits']:
         the_id = hit['_id'].encode('UTF-8')
-        document_length = len(hit['_source']['text'].split(' '))
+        # document_length = len(hit['_source']['text'].split(' '))
         # print document_length
-        total += document_length
-        num += 1
+        # total += document_length
+        # num += 1
         docno = hit['_source']['docno'].encode('UTF-8')
         tf = hit['_explanation']['details'][0]['details'][0]['details'][0]['value']
         temp = {
-            'document_length':document_length,
+            'document_length': document_length[docno],
             'docno': docno,
             'tf': tf,
             'df': df
         }
         d = Document(the_id, temp)
-        # print d.information
         output.append(d)
-    # posting and the average length
-    if num == 0:
-        return [output, 0]
-    return [output, total / num]
+    return output
 # ----------------------------------------------------------
 def termFrequencyInQuery(term, query):
     freq = 0
@@ -52,29 +97,37 @@ def termFrequencyInQuery(term, query):
         if t == term:
             freq += 1
     return freq
-def Okapi_TF(d, avg):
+def Okapi_TF(d):
+    global average
     tf = d.information['tf']
     # TODO double-check if the length is correct
     length = d.information['document_length']
-    return tf / (tf + 0.5 + 1.5 * (length / avg))
+    return tf / (tf + 0.5 + 1.5 * (length / average))
 # ----------------------------------------------------------
-def TF_IDF(d, avg):
-    return Okapi_TF(d, avg) * math.log10(D / d.information['df'])
+def TF_IDF(d):
+    df = d.information['df']
+    return Okapi_TF(d) * math.log10(D / df)
 # ----------------------------------------------------------
-def Okapi_BM25(tfq, d, avg):
+def Okapi_BM25(tfq, d):
     k1 = 1.2
-    k2 = 100
+    k2 = 100.0
     b = 0.75
+    global average
     tf = d.information['tf']
     df = d.information['df']
     length = d.information['document_length']
     one = math.log10((D + 0.5) / (df + 0.5))
-    two = (tf + k1 * tf) / (tf + k1 * (1 - b + b * length / avg))
+    two = (tf + k1 * tf) / (tf + k1 * (1 - b + (b * length) / average))
     three = (tfq + k2 * tfq) / (tfq + k2)
     return one * two * three
 def Laplace(d):
     tf = d.information['tf']
-
+    length = d.information['document_length']
+    p = (tf + 1) / (length + V)
+    return math.log10(p)
+def Jelinek_Mercer(d):
+    f = 0.3
+    tf = d.information['tf']
 # ----------------------------------------------------------
 def getPostings(keyword):
     result = es.search(index = 'ap_dataset', doc_type = 'document',
@@ -93,40 +146,30 @@ def sort(data, k):
     # sort the dictionary, and return the first k pairs
    result = sorted(data.items(), key=lambda x:(-x[1],x[0]))
    return list(islice(result, k))
+# def sort1(data, k):
+
 # ----------------------------------------------------------
 def search(query, k):
     Okapi_TF_scores = collections.defaultdict(lambda:0.0)
     TF_IDF_scores = collections.defaultdict(lambda:0.0)
+    Okapi_BM25_socres = collections.defaultdict(lambda:0.0)
+    Laplace_scores = collections.defaultdict(lambda:0.0)
     for item in query:
         posting = getPostings(item)
-        avg = posting[1]
         # term frequency in query
         freq = termFrequencyInQuery(item, query)
-        for doc in posting[0]:
+        for doc in posting:
             docno = doc.information['docno']
-            Okapi_TF_scores[docno] += Okapi_TF(doc, avg)
-            TF_IDF_scores[docno] += TF_IDF(doc, avg)
-            # if model == 'TF-IDF':
-            #     scores[doc.information['docno']] += TF_IDF(doc, avg)
-            #     continue
-            # if model == 'Okapi BM25':
-            #     scores[doc.information['docno']] += Okapi_BM25(freq, doc, avg)
-            #     continue
-            # if model == 'Unigram LM with Laplace smoothing':
-            #     # TODO
-            #     continue
-            # if model == 'Unigram LM with Jelinek-Mercer smoothing':
-            #     # TODO
-            #     continue
+            Okapi_TF_scores[docno] += Okapi_TF(doc)
+            TF_IDF_scores[docno] += TF_IDF(doc)
+            Okapi_BM25_socres[docno] += Okapi_BM25(freq, doc)
+            Laplace_scores[docno] += Laplace(doc)
     Okapi_TF_scores = sort(Okapi_TF_scores, k)
     TF_IDF_scores = sort(TF_IDF_scores, k)
-    return [Okapi_TF_scores, TF_IDF_scores]
+    Okapi_BM25_socres = sort(Okapi_BM25_socres, k)
+    Laplace_scores = sort(Laplace_scores, k)
+    return [Okapi_TF_scores, TF_IDF_scores, Okapi_BM25_socres, Laplace_scores]
     # print scores
-searchlist = ['sex']
-model = ['Okapi TF', 'TF-IDF', 'Okapi BM25',
-         'Unigram LM with Laplace smoothing',
-         'Unigram LM with Jelinek-Mercer smoothing']
-
 # ----------------------------------------------------------
 def stringOperation(q):
     query = q[q.find('Document'):len(q)-1]
@@ -185,19 +228,50 @@ def doSearch():
     questions = getQuestion()
     Okapi_TF = []
     TF_IDF = []
-    # Okapi_BM25 = []
-    # Laplace_smoothing = []
+    Okapi_BM25 = []
+    Laplace_smoothing = []
     # Jelinek_Mercer = []
     for index in questions:
+        print 'search ', questions[index]
         temp = ''
         temp += index + ' Q0 '
         result = search(questions[index], K)
         Okapi_TF = outputFormat(Okapi_TF, result[0], temp)
         TF_IDF = outputFormat(TF_IDF, result[1], temp)
+        Okapi_BM25 = outputFormat(Okapi_BM25, result[2], temp)
+        Laplace_smoothing = outputFormat(Laplace_smoothing, result[3], temp)
     writeFile(Okapi_TF, 'Okpai_TF')
     writeFile(TF_IDF, 'TF_IDF')
+    writeFile(Okapi_BM25, 'Okapi_BM25')
+    writeFile(Laplace_smoothing, 'Laplace_smoothing')
+def getNeededInformationFromFile():
+    f = open('cache', 'r')
+    global V
+    global average
+    global document_length
+    content = []
+    for line in f:
+        # extract \n
+        line = line[:-1]
+        content.append(line)
+    f.close()
+    V = int(content[0])
+    average = float(content[1])
+    i = 2
+    for line in content:
+        if 'AP' not in line:
+            continue
+        line = line.split(' ')
+        docno = line[0]
+        length = int(line[1])
+        document_length[docno] = length
 
 def main():
+    print 'get D, V, average document length'
+    # getNeededInformation()
+    getNeededInformationFromFile()
+    print D, V, average
+    print 'start to perform search'
     doSearch()
-if __name__ '__main__':
+if __name__ == '__main__':
     main()
