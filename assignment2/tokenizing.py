411609d90d2003e06a9516881af5a2b3a7339f86
zhaoze1991 <zhaoze.chou@gmail.com>
2015-02-19 18:19:43 -0500

    init for indexing

--- assignment2/tokenizing.py
+++ assignment2/tokenizing.py
@@ -1,11 +1,23 @@
 #!/usr/local/bin/python
-import os, sys, re
+import os, sys, re, collections
 import glob
 import snowballstemmer
 import documents
 # data definition
 files = []
 path = './AP_DATA/ap89_collection/*'
+# term -> id
+term_id = {}
+# id -> term
+id_term = {}
+# term -> [(docno, position)]
+hash_map = {}
+space = ' '
+cf = {}
+df = {}
+vf = {}
+document_id = 0
+document_num = 0
 
 def getFileName():
     global files
@@ -21,11 +33,6 @@ def readFile(fileName):
     f.close()
     return content
 
-
-termid = {}
-_id = 1
-document_id = 1
-
 def stem(word):
     # static variable
     stem.stemmer = snowballstemmer.stemmer('english')
@@ -33,32 +40,98 @@ def stem(word):
     return word
 
 def tokenizing(documents):
+    global document_id
     for document in documents:
+        tf = {}
+        words = {}
         docno = document[0]
         content = document[1]
-        pattern = re.compile('\w+\.?\w*')
+        pattern = re.compile('\w+(?:\.?\w+)*')
         tokens = pattern.findall(content)
         result = []
         position = 1
-        global _id
+        print document_id
+        id_term[document_id] = docno
         for t in tokens:
             t = t.lower()
-            if '.' == t[len(t) - 1]:
-                t = t[:-1]
-            if t in termid:
-                tuples = (t, docno, termid[t], position)
-                result.append(tuples)
+            if t in words:
+                words[t].append(position)
+                tf[t] += 1
             else:
-                termid[t] = _id
-                _id += 1
-                tuples = (t, docno, termid[t], position)
-                result.append(tuples)
+                words[t] = [position]
+                tf[t] = 1
+                # unique term in documents
+                if t in vf:
+                    pass
+                else:
+                    vf[t] = 1
+            if t in cf:
+                cf[t] += 1
+            else:
+                cf[t] = 1
             position += 1
-        print result
+        for key in words:
+            # key is the term
+            if key in df:
+                df[key] += 1
+            else:
+                df[key] = 1
+            if key in hash_map:
+                tuples = (tf[key], ) + tuple(words[key])
+                # hash_map[key] = {}
+                hash_map[key][document_id] = tuples
+            else:
+                tuples = (tf[key], ) + tuple(words[key])
+                hash_map[key] = {}
+                hash_map[key][document_id] = tuples
+        document_id += 1
+
+def sort():
+    global hash_map
+    result = sorted(hash_map.items(), key=lambda x: x[0])
+    return result
+
+def writefile(fileName, content):
+    cache = open(fileName, 'wa')
+    category = open(fileName+'Category','wa')
+    start = 0
+    for block in content:
+        # (term, blabla)
+        term = block[0]
+        string = term + space + str(df[term]) + space + str(cf[term]) + space
+        string += str(block[1]) + '\n'
+        cache.write(string)
+        cate = term + space + str(start) + space + str(len(string)) + '\n'
+        category.write(cate)
+        start += len(string)
+    cache.close()
+    category.close()
+
+
+
+
+def indexing():
+    result = sort()
+    if os.path.exists('cache1') == False:
+        writefile('cache1', result)
+        return
+    if os.path.exists('cache2') == False:
+        writefile('cache2', result)
+        # mergefile()
+        sys.exit(-1)
+
 
 getFileName()
+result = []
 for f in files:
     c = readFile(f)
-    documents = documents.splitDoc(c)
-    tokenizing(documents)
-    sys.exit(-1)
+    document = documents.splitDoc(c)
+    size = len(document)
+    if document_num + size >= 1000:
+        indexing()
+        document_num = 0
+        hash_map = {}
+        df = {}
+    else:
+        document_num += size
+        tokenizing(document)
