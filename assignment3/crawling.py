805c90a79830feab5d8f7915913a3af306f9d95a
zhaoze1991 <zhaoze.chou@gmail.com>
2015-03-20 17:15:05 -0400

    ignore all the network error, simply drop the url

--- assignment3/crawling.py
+++ assignment3/crawling.py
@@ -18,23 +18,9 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+
+# data definition
 es = Elasticsearch()
-
-def dump_to_es(url, html, clean_text, header):
-    doc = {
-    'url':url,
-    'html':html,
-    'text':clean_text,
-    'header':header
-    }
-    es.index(index = 'hw3',
-        doc_type = 'document',
-        id = uuid.uuid5(uuid.NAMESPACE_URL, url),
-        body =doc
-        )
-    # print 'index'
-    pass
-
 counter = 0
 seeds = [
     'http://en.wikipedia.org/wiki/List_of_highest-grossing_films',
@@ -42,8 +28,29 @@ seeds = [
     'http://en.wikipedia.org/wiki/Avatar_(2009_film)',
     'http://www.imdb.com/title/tt0499549/'
 ]
+skip_format = [
+    '.jpg',
+    '.JPG',
+    '.svg',
+    '.SVG',
+    '.png',
+    '.PNG'
+]
 
-f = open('storage', 'w')
+
+def dump_to_es(url, html, clean_text, header):
+    doc = {
+        'url': url,
+        'html': html,
+        'text': clean_text,
+        'header': header
+    }
+    es.index(index='hw3',
+             doc_type='document',
+             id=str(uuid.uuid5(uuid.NAMESPACE_URL, url)),
+             body=doc)
+    pass
+
 
 class Link(object):
     """docstring for Link"""
@@ -76,41 +83,24 @@ spam_map = {}  # url -> whatever, should store this to filter un-related documen
 domain_time = {}  # domain -> last time visit
 
 
-def get_header(urls):
-    # return language and content type
-    req = urllib2.Request(urls)
-    # req.get_method = lambda: 'HEAD'
-    information = urllib2.urlopen(req).info()
-    return [information.getheader('content-language'), information.getheader('content-type'), information]
-
-
 def check_crawler(url):
     # check if the url can be visited
-    rp = robotparser.RobotFileParser()
-    domain = get_domain(url)
-    protocol = get_protocol(url) + '://'
-    bots_location = protocol + domain
-    rp.set_url(bots_location + '/robots.txt')
-    rp.read()
-    return rp.can_fetch("*", url)
+    try:
+        rp = robotparser.RobotFileParser()
+        domain = get_domain(url)
+        protocol = get_protocol(url) + '://'
+        bots_location = protocol + domain
+        rp.set_url(bots_location + '/robots.txt')
+        rp.read()
+        return rp.can_fetch("*", url)
+    except:
+        return False
 
 
 def url_normalization(current_url, url):
     # TODO make this function better
     norm = urlnorm.norms(url)
     return norm
-
-
-def write_to_file(docno, title, content):
-    f.writelines('<DOC>\n')
-    f.writelines('<DOCNO> ' + docno + ' </DOCNO>\n')
-    if title is not None:
-        # todo may be there's bug
-        f.writelines('<HEAD> ' + title + ' </HEAD>\n')
-    f.writelines('<TEXT>\n')
-    f.writelines(content)
-    f.writelines('</TEXT>\n')
-    f.writelines('</DOC>\n')
 
 
 def get_protocol(url):
@@ -148,6 +138,13 @@ def sleep_function(url):
     pass
 
 
+def filtered(url, content):
+    if 'film' in url or 'movie' in 'url' or 'film' in content or 'movie' in content or 'imdb' in url:
+        return False
+    return True
+    pass
+
+
 def fetch_page(url):
     # todo change the check sequence
     urls = url.url
@@ -157,46 +154,40 @@ def fetch_page(url):
         return
     try:
         response = urllib2.urlopen(urls).read() # this is the raw html
+        header = wtf.get_header(urls)
     except:
         return
-    header = wtf.get_header(urls)
+    # skip not english and not html
     if header[0] != 'en' or 'text/html' not in header[1]:
         return
     soup = bs4.BeautifulSoup(response)
     title = soup.title.string.encode('utf-8')
     clean_text = wtf.clean_text(soup.findAll(text=True))
-    try:
-        clean_text = readability.clean_text(urls).encode('utf-8')
-    except:
+    small = clean_text.lower()
+    if filtered(urls.lower(), small):
         return
-    # clean_text_low = clean_text.lower()
-    # print clean_text_low
-    # if 'film' not in clean_text_low and 'movie' not in clean_text_low:
-    #     spam_map[urls] = True
-    #     return
     global counter
     counter += 1
-    print counter
+    print 'dumped: ', counter
     dump_to_es(urls, response, clean_text, header[2])
-    # write_to_file(urls, title, clean_text)
     for link in soup.find_all('a'):
         link = link.get('href')
         # TODO we can filter more
         if link is None:
             continue
         temp = link.encode('utf-8')
-        if len(temp) < 1:
+        if len(temp) < 1 or temp[0] == '#':
             continue
-        if temp[0] == '#':
+        try:
+            temps = url_formatter(urls, temp)
+        except:
+            print temps
             continue
-        temps = url_formatter(urls, temp)
         if temps in hash_map:
             hash_map[temps].in_link += 1
             hash_map[temps].ins.append(urls)
             continue
-        if '.jpg' in temps or '.JPG' in temps or '.png' in temps or '.PNG' in temps:
-            continue
-        if '.svg' in temps or '.SVG' in temps:
+        if '.jpg' in temps or '.JPG' in temps or '.png' in temps or '.PNG' in temps or '.svg' in temps or '.SVG' in temps:
             continue
         hash_map[temps] = Link(temps)
         url.outs.append(temps)
@@ -205,45 +196,19 @@ def fetch_page(url):
         q.push(hash_map[temps])
 
 
-def filter_mechanism(url):
-    if url in spam_map:
-        return True
-    temp_url = url.lower()
-    if 'film' in temp_url or 'movie' in temp_url:
-        return False
-    try:
-        sleep_function(url)
-        header = get_header(url)
-        if header[0] != 'en' or 'text/html' not in header[1]:
-            spam_map[url] = True
-            return True
-    except:
-        spam_map[url] = True
-        return True
-    spam_map[url] = True
-    return True
-    pass
-
-
 def update_es(url, ins, outs):
-    inns = ''
-    ouuts = ''
-    for i in ins:
-        inns += i + ' '
-    for i in outs:
-        ouuts += i + ' '
+    inns = map(wtf.url_to_uuid, ins)
+    ous = map(wtf.url_to_uuid, outs)
     doc = {
-        'doc':{
+        'doc': {
             'in-links': inns,
-            'out-links' : ouuts
+            'out-links': ous
         }
     }
-    # print doc
-    es.update(index = 'hw3',
-        id = str(uuid.uuid5(uuid.NAMESPACE_URL, url)),
-        doc_type = 'document',
-        body = doc
-        )
+    es.update(index='hw3',
+              id=str(uuid.uuid5(uuid.NAMESPACE_URL, url)),
+              doc_type='document',
+              body=doc)
     pass
 
 
@@ -253,17 +218,14 @@ if __name__ == '__main__':
     for seed in seeds:
         hash_map[seed] = Link(seed)
         q.push_seeds(hash_map[seed])
-    while not q.empty() and counter < 5:
+    while not q.empty() and counter < 12000:
         element = q.pop()
         fetch_page(element)
         pass
     for url in hash_map:
         if len(hash_map[url].outs) < 1:
             continue
-        link_map.writelines(url + ' ')
-        for links in hash_map[url].outs:
-            link_map.writelines(links + ' ')
-        link_map.writelines('\n')
+        update_es(hash_map[url].url, hash_map[url].ins, hash_map[url].outs)
 
 
 
