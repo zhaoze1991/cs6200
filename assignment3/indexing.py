452bdf4e87f2317528d342311b752f5d0b9a1bb7
zhaoze1991 <zhaoze.chou@gmail.com>
2015-03-31 12:28:49 -0400

    add exist check

--- assignment3/indexing.py
+++ assignment3/indexing.py
@@ -6,8 +6,10 @@ import wtf
 from elasticsearch import Elasticsearch
 es = Elasticsearch()
 # data definition
-f = open('storage_12573', 'r').readlines()
-l = open('linkgraph_12573', 'r').readlines()
+f = open('storage_3916', 'r').readlines()
+inlink = open('inlinks2.txt', 'r').readlines()
+l = open('linkgraph_3916', 'r').readlines()
+
 hash_map = {}
 
 class URL(object):
@@ -20,6 +22,11 @@ class URL(object):
         self.in_links = []
         self.out_links = []
 
+def exists(url):
+    return es.exists(index = 'hw3',
+        id = wtf.url_to_uuid(url),
+        doc_type = 'document')
+
 
 def handleTemp(temp, tag, end_tag):
     # extract content from <TEXT> and void <TEXT>content</TEXT>
@@ -27,9 +34,29 @@ def handleTemp(temp, tag, end_tag):
     end = temp.find(end_tag)
     return temp[start:end]
 
+def merge(url, title, http_header, text, raw):
+    existed = es.get(index = 'hw3', doc_type = 'document', id =wtf.url_to_uuid(url)
+                fields=['in-links'])['fields']['in-links']
+    res = set()
+    for url in hash_map[url].in_links:
+        res.add(url.encode('utf-8'))
+    for url in existed:
+        res.add(wtf.url_to_uuid(url))
+    doc = {
+            'doc': {
+                'in-links': res
+            }
+        }
+    es.update(index='hw3',
+                  id=wtf.url_to_uuid(url),
+                  doc_type='document',
+                  body=doc)
+    for
 # ----------------------------------------------------------------------------
 def indexing(url, title, http_header, text, raw):
-    print url
+    if check_exist(url):
+        merger(url, title, http_header, text, raw)
+        return
     if url not in hash_map:
         print 'not '
     # in_links = map(wtf.url_to_uuid, hash_map[url].in_links)
@@ -85,9 +112,16 @@ def main():
         url = line[0]
         hash_map[url] = URL(url)
         hash_map[url].out_links = line[1 : len(line)]
+    for line in inlink:
+        line = line.split()
+        if len(line) < 1:
+            continue
+        url = line[0]
+        hash_map[url].in_links = line[1 :len(line)]
         # print hash_map[url].in_links, hash_map[url].out_links
     splitDoc(f)
     # print count
 if __name__ == '__main__':
     main()
+    # exists('http://en.wikipedia.org/wiki/List_of_highest-grossing_films')
     # print check_exist('http://en.wikipedia.org/wiki/Rhys_Ifans')
